6dbfc043d23f136411b430a9f1f7ff628ada6a00
team3hailong <LongTH.B23CN510@stu.ptit.edu.vn>
2025-05-22 10:35:39 +0700

    update

--- private/config/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-// RTK API configuration
-
-define('API_ACCESS_KEY', 'Zb5F6iKUuAISy4qY');
-define('API_SECRET_KEY', 'KL1KEEJj2s6HA8LB');
-
-define('API_BASE_URL', 'http://203.171.25.138:8090');
-define('API_SIGN_METHOD', 'HmacSHA256');
